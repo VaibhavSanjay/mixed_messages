4f64d717fa8e7ea791c0c1a13ec09b2a4d09b819
VaibhavSanjay <vsanjay@terpmail.umd.edu>
2022-01-12 12:32:03 -0500

    Add Comments to mixed_messages.js and updated README

--- README.txt
+++ README.txt
@@ -1,1 +1,3 @@
 Mixed Messages Project from CodeAcademy
+
+The greet() function returns a string of the form "Good [Time of Day], my [person] [person limb]s are [adjective]".
--- mixed_messages.js
+++ mixed_messages.js
@@ -2,10 +2,13 @@
 const day = ['morning', 'afternoon', 'evening'];
 // People who greet
 const greeter = ['robot', 'dog', 'human'];
+// Limbs based on the greeted person.
 const greeterLim = [['circuit', 'sensor'], ['paw', 'ear'], ['arm', 'toe']];
 // Adjectives
 const adj = ['aching', 'restless', 'normal', 'perky'];
 
+// Chooses a random integer between 0 and n - 1 inclusive
 const r = n => Math.floor(Math.random()*n);
+// Returns random string of the form "Good [Time of Day], my [person] [person limb]s are [adjective]"
 const greet = () => `Good ${day[r(day.length)]}, my ${greeter[x = r(greeter.length)]} ${greeterLim[x][r(greeterLim[x].length)]}s are ${adj[r(adj.length)]}.`;
 console.log(greet());
